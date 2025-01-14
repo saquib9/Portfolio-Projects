b0c3b3d5007934afcd882fd66b67b1f2e2ff3668
Saquib <saquibsmac@tintins-laptop.mynetworksettings.com>
2025-01-14 03:02:22 -0500

    updated price prediction

---
+++ Walmart-Sales-Analysis/README.md
@@ -0,0 +1,58 @@
+# Walmart Sales Data Analysis
+## About
+This project aims to explore Walmart Sales data to understand top-performing branches and products, sales trends of of different products, and customer behavior. This project aims to gain insight into Walmart's sales data to understand the factors affecting sales in different branches.
+
+## Analysis List
+#### 1. Product Analysis
+Analyze the data to understand the different product lines, the ones performing best, and the ones that need improvement.
+
+#### 2. Customer Analysis
+This analysis aims to uncover the different customer segments, purchase trends, and the profitability of each customer segment.
+
+#### 3. Sales Analysis
+This analysis aims to answer the question of sales trends. The result can help us measure the effectiveness of each sales strategy the business applies and the modifications needed to increase sales.
+
+## Approach Followed
+### 1. Data Wrangling: 
+This is the first step where data inspection is done to confirm that NULL values and missing values are detected and data replacement methods are used to replace, missing or NULL values.
+1. Build a database
+2. Create the table and insert the data.
+3. Select columns with null values in them. There are no null values in our database as in creating the tables, we set NOT NULL for each field, hence null values are filtered out.
+### 2. Feature Engineering: 
+This will help us generate some new columns from existing ones.
+1. Add a new column named time_of_day to give an insight into sales in the Morning, Afternoon, and Evening. This will help answer the question of which part of the day most sales are made.
+2. Add a new column named day_name that contains the extracted days of the week on which the given transaction took place (Mon, Tue, Wed, Thur, Fri). This will help answer the question of which week of the day each branch is busiest.
+3. Add a new column named month_name that contains the extracted months of the year on which the given transaction took place (Jan, Feb, Mar). Determine which month of the year has the most sales and profit.
+### 3. Exploratory Data Analysis (EDA): 
+Exploratory data analysis is done to answer the questions listed below.
+
+#### A. Generic Questions
+1. How many unique cities does the data have?
+2. In which city is each branch?
+#### B. Product-Related Questions
+3. How many unique product lines does the data have?
+4. What is the most common payment method?
+5. What is the most selling product line?
+6. What is the total revenue by month?
+7. What month had the largest COGS?
+8. What product line had the largest revenue?
+9. Fetch each product line and add a column to those product lines showing "Good", and "Bad". Good if it is greater than average sales
+10. Which branch sold more products than the average product sold?
+11. What is the most common product line by gender?
+12. What is the average rating of each product line?
+#### C. Sales-Related Questions
+13. Number of sales made at each time of the day per weekday
+14. Which of the customer types brings the most revenue?
+15. Which city has the largest tax percentage/ VAT (Value Added Tax)?
+#### D. Customer-Related Questions
+16. What is the most common customer type?
+17. What is the gender of most of the customers?
+18. What is the gender distribution per branch?
+19. Which time of the day do customers give the most ratings?
+20. Which time of the day do customers give the most ratings per branch?
+21. Which day of the week has the best average ratings?
+22. Which day of the week has the best average ratings per branch?
+
+
+
+
---
+++ Walmart-Sales-Analysis/walmartSalesAnalysis.sql
@@ -0,0 +1,219 @@
+/*
+Project: Walmart Sales Analysis
+Query Language: MySQL
+Dataset: https://github.com/saquib9/Portfolio-Projects/blob/main/Portfolio-Projects/Walmart-Sales-Analysis/WalmartSalesData.csv
+*/ 
+
+CREATE DATABASE IF NOT EXISTS walmart_sales_data;
+CREATE TABLE IF NOT EXISTS sales(
+	invoice_id VARCHAR(30) NOT NULL PRIMARY KEY,
+    branch VARCHAR(5) NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    customer_type VARCHAR(30) NOT NULL,
+    gender VARCHAR(30) NOT NULL,
+    product_line VARCHAR(100) NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    tax_5_percent FLOAT NOT NULL,
+    total DECIMAL(12, 4) NOT NULL,
+    date DATETIME NOT NULL,
+    time TIME NOT NULL,
+    payment VARCHAR(15) NOT NULL,
+    cogs DECIMAL(10,2) NOT NULL, -- COGS = Cost of Goods Sold 
+    gross_margin_percentage FLOAT,
+    gross_income DECIMAL(12, 4),
+    rating FLOAT
+);
+
+-- -------------------------------------------------------------------
+-- ------------------------Feature Engineering------------------------
+-- -------------------------------------------------------------------
+
+-- Added column 'time_of_day' 
+ALTER TABLE sales ADD COLUMN time_of_day VARCHAR(9);
+
+UPDATE sales SET time_of_day = (
+	CASE 
+		WHEN time BETWEEN '00:00:00' AND '11:59:59' THEN 'Morning'
+        WHEN time BETWEEN '12:00:00' AND '15:59:59' THEN 'Afternoon'
+        WHEN time BETWEEN '16:00:00' AND '18:59:59' THEN 'Evening'
+        ELSE 'Night'
+	END
+);
+
+-- Added column 'day_name' 
+ALTER TABLE sales ADD COLUMN day_name VARCHAR(9);
+
+UPDATE sales SET day_name = DAYNAME(date);
+
+-- Added column 'month_name' 
+ALTER TABLE sales ADD COLUMN month_name VARCHAR(8);
+
+UPDATE sales SET month_name = MONTHNAME(date);
+
+-- -----------------------------------------------------------------
+-- ---------------- Solution to Business Questions -----------------
+-- -----------------------------------------------------------------
+
+-- ------------------------- 1. General ----------------------------
+
+-- How many unique cities does the data have?
+SELECT COUNT(DISTINCT city) AS TOTAL_CITIES from sales;
+
+-- Location (City) of each branch
+SELECT DISTINCT branch, city FROM sales;
+
+-- ------------------------- 2. Product ----------------------------
+
+-- How many unique product lines does the data have?
+SELECT COUNT(DISTINCT product_line) FROM sales;
+
+-- What is the most common payment method?
+SELECT payment, COUNT(payment) AS pay_method_count from sales 
+GROUP BY payment ORDER BY pay_method_count DESC;
+
+-- What is the most selling product line?
+SELECT product_line, COUNT(product_line) AS prd_line_count from sales 
+GROUP BY product_line ORDER BY prd_line_count DESC;
+
+-- What is the total revenue by month?
+SELECT month_name, SUM(total) AS revenue from sales 
+GROUP BY month_name ORDER BY revenue DESC;
+
+-- What month had the largest COGS?
+SELECT month_name, SUM(cogs) AS monthly_cogs from sales 
+GROUP BY month_name ORDER BY monthly_cogs DESC;
+
+-- What product line had the largest revenue?
+SELECT product_line, SUM(total) AS revenue from sales 
+GROUP BY product_line ORDER BY revenue DESC;
+
+<<<<<<< HEAD
+-- Fetch each product line and add a column to those product lines 
+-- showing "Good", and "Bad". Good if it is greater than average sales
+=======
+-- Fetch each product line and add a column showing "Good", and "Bad". Good if it is greater than average sales.
+>>>>>>> 0d610820ba1776d6e22e92adb1c10eee8427e9e3
+
+WITH total_sales AS (
+	SELECT product_line, SUM(total) AS total
+	FROM sales GROUP BY product_line),
+avg_sales AS (
+    SELECT AVG(total) AS avg_sales
+    FROM total_sales
+)
+SELECT
+    product_line,
+    CASE
+        WHEN total > (SELECT avg_sales FROM avg_sales) THEN 'Good'
+        ELSE 'Bad'
+    END AS remark
+FROM total_sales
+GROUP BY product_line;
+
+-- Which branch sold more products than the average product sold?
+
+WITH branch_totals AS (
+	SELECT branch, SUM(quantity) AS total_quantity
+	FROM sales GROUP BY branch),
+average_branch_sales AS (
+	SELECT AVG(total_quantity) as avg_quantity
+    FROM branch_totals)
+SELECT branch, total_quantity FROM branch_totals
+WHERE total_quantity > (SELECT avg_quantity FROM average_branch_sales);
+
+-- What is the most common product line by gender?
+SELECT
+	gender,
+    product_line,
+    COUNT(gender) AS total_cnt
+FROM sales
+GROUP BY gender, product_line
+ORDER BY total_cnt DESC;
+
+-- What is the average rating of each product line?
+SELECT product_line,
+	ROUND(AVG(rating), 2) as avg_rating
+FROM sales GROUP BY product_line ORDER BY avg_rating DESC;
+
+SELECT
+    day_name,
+    time_of_day,
+    COUNT(*) AS total_sales
+FROM sales
+GROUP BY day_name, time_of_day
+ORDER BY day_name, total_sales DESC;
+
+-- --------------------------- 3. Sales --------------------------------
+
+-- Number of sales made at each time of the day per weekday
+SELECT
+    day_name,
+    time_of_day,
+    COUNT(*) AS total_sales
+FROM sales
+WHERE day_name NOT IN ('Saturday', 'Sunday')
+GROUP BY day_name, time_of_day
+ORDER BY day_name, total_sales DESC;
+
+-- Which of the customer types brings the most revenue?
+SELECT
+	customer_type,
+	SUM(total) AS total_revenue
+FROM sales
+GROUP BY customer_type
+ORDER BY total_revenue;
+
+-- Which city has the largest tax percentage/ VAT (Value Added Tax)?
+SELECT city,
+    ROUND(AVG(tax_5_percent*100/total), 2) AS avg_tax_percent
+FROM sales
+GROUP BY city ORDER BY avg_tax_percent DESC;
+
+-- ------------------------ 4. Customers -----------------------------
+
+-- What is the most common customer type?
+SELECT customer_type, COUNT(*) AS count
+FROM sales 
+GROUP BY customer_type ORDER BY count DESC;
+
+-- What is the gender of most of the customers?
+SELECT	gender, COUNT(*) AS gender_count
+FROM sales
+GROUP BY gender ORDER BY gender_count DESC;
+
+-- What is the gender distribution per branch?
+SELECT branch, gender, COUNT(*) as gender_count
+FROM sales
+GROUP BY branch, gender ORDER BY branch, gender DESC;
+
+-- Which time of the day do customers give the most ratings?
+SELECT time_of_day,
+	AVG(rating) AS avg_rating
+FROM sales
+GROUP BY time_of_day ORDER BY avg_rating DESC;
+
+-- Which time of the day do customers give the most ratings per branch?
+SELECT	branch, time_of_day,
+	AVG(rating) AS avg_rating
+FROM sales
+GROUP BY branch, time_of_day ORDER BY avg_rating DESC;
+
+-- Which day of the week has the best average ratings?
+SELECT day_name,
+	AVG(rating) AS avg_rating
+FROM sales
+GROUP BY day_name ORDER BY avg_rating DESC;
+
+-- Which day of the week has the best average ratings per branch?
+SELECT branch, day_name,
+	AVG(rating) AS avg_rating
+FROM sales
+GROUP BY branch, day_name ORDER BY avg_rating DESC;
+
+
+
+
+
+
+
